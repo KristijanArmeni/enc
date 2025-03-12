4ddbe790be9324deb14782c62f12469be9d4683b
KristijanArmeni <kristijan.armeni@gmail.com>
2025-03-12 15:51:27 -0400

    [MAINT] remove globals().update, use args explicitly, fix imports

--- src/lebel_encoding/run_all_replication.py
+++ src/lebel_encoding/run_all_replication.py
@@ -5,13 +5,15 @@ import os
 import pathlib
 import sys
 from os.path import dirname, join
+from pathlib import Path
 
 import h5py
 import numpy as np
+from scipy.stats import sem
 
 from encoders.utils import check_make_dirs, create_run_folder_name, load_config
 from lebel_encoding.config import EM_DATA_DIR, REPO_DIR
-from lebel_encoding.encoding_utils import *
+from lebel_encoding.encoding_utils import apply_zscore_and_hrf, get_response
 from lebel_encoding.feature_spaces import _FEATURE_CONFIG, get_feature_space
 from lebel_encoding.ridge_utils.ridge import bootstrap_ridge
 
@@ -29,17 +31,17 @@ if __name__ == "__main__":
     parser.add_argument("--chunklen", type=int, default=10)
     parser.add_argument("--nchunks", type=int, default=10)
     parser.add_argument("--singcutoff", type=float, default=1e-10)
-    parser.add_argument("-use_corr", action="store_true")
-    parser.add_argument("-single_alpha", action="store_true")
+    parser.add_argument("--use_corr", action="store_true")
+    parser.add_argument("--single_alpha", action="store_true")
+    parser.add_argument("--run_folder_name", type=str)
     logging.basicConfig(level=logging.INFO)
 
     args = parser.parse_args()
-    globals().update(args.__dict__)
 
     RUNS_DIR = load_config()["RUNS_DIR"]
 
     fs = " ".join(_FEATURE_CONFIG.keys())
-    assert feature in _FEATURE_CONFIG.keys(), "Available feature spaces:" + fs
+    assert args.feature in _FEATURE_CONFIG.keys(), "Available feature spaces:" + fs
 
     train_stories, test_stories = [], []
     with open(join(EM_DATA_DIR, "sess_to_story.json"), "r") as f:
@@ -53,27 +55,35 @@ if __name__ == "__main__":
     assert len(set(train_stories) & set(test_stories)) == 0, "Train - Test overlap!"
     allstories = list(set(train_stories) | set(test_stories))
 
-    if isinstance(n_train_stories, int):
-        n_train_stories_list = [n_train_stories]
+    if isinstance(args.n_train_stories, int):
+        n_train_stories_list = [args.n_train_stories]
     else:
-        n_train_stories_list = n_train_stories
+        n_train_stories_list = args.n_train_stories
 
-    folder_name = create_run_folder_name()
-    base_dir = os.path.join(RUNS_DIR, folder_name)
+    # if run folder name not given, create one
+    if not args.run_folder_name:
+        run_folder_name = create_run_folder_name()
+    else:
+        run_folder_name = args.run_folder_name
+
+    run_folder = os.path.join(RUNS_DIR, run_folder_name)
+    Path(run_folder).mkdir(parents=True, exist_ok=True)
+
     for current_n_train_stories in n_train_stories_list:
+        # e.g. /UTS01/embeddings/5/not_shuffled/scores_mean.py
         output_dir = os.path.join(
-            base_dir,
-            "replication",
-            subject,
+            run_folder,
+            args.subject,
+            "embeddings",  # only option here
             str(current_n_train_stories),
             "not_shuffled",
         )
-        check_make_dirs(output_dir, verbose=False, isdir=True)
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
         print("Saving encoding model & results to:", output_dir)
 
-        downsampled_feat = get_feature_space(feature, allstories)
+        downsampled_feat = get_feature_space(args.feature, allstories)
         print("Stimulus & Response parameters:")
-        print("trim: %d, ndelays: %d" % (trim, ndelays))
+        print("trim: %d, ndelays: %d" % (args.trim, args.ndelays))
 
         # result arrays
         scores_list = list()
@@ -81,7 +91,7 @@ if __name__ == "__main__":
         best_alphas_list = list()
 
         rng = np.random.default_rng()
-        for repeat in range(n_repeats):
+        for repeat in range(args.n_repeats):
             print("Repeat %d" % repeat)
             print(train_stories)
             # choose stories to sample for this repeat
@@ -92,18 +102,18 @@ if __name__ == "__main__":
 
             # Delayed stimulus
             delRstim = apply_zscore_and_hrf(
-                curr_train_stories, downsampled_feat, trim, ndelays
+                curr_train_stories, downsampled_feat, args.trim, args.ndelays
             )
             print("delRstim: ", delRstim.shape)
             delPstim = apply_zscore_and_hrf(
-                curr_test_stories, downsampled_feat, trim, ndelays
+                curr_test_stories, downsampled_feat, args.trim, args.ndelays
             )
             print("delPstim: ", delPstim.shape)
 
             # Response
-            zRresp = get_response(curr_train_stories, subject)
+            zRresp = get_response(curr_train_stories, args.subject)
             print("zRresp: ", zRresp.shape)
-            zPresp = get_response(curr_test_stories, subject)
+            zPresp = get_response(curr_test_stories, args.subject)
             print("zPresp: ", zPresp.shape)
 
             # Ridge
@@ -112,7 +122,13 @@ if __name__ == "__main__":
             print("Ridge parameters:")
             print(
                 "nboots: %d, chunklen: %d, nchunks: %d, single_alpha: %s, use_corr: %s"
-                % (nboots, chunklen, nchunks, single_alpha, use_corr)
+                % (
+                    args.nboots,
+                    args.chunklen,
+                    args.nchunks,
+                    args.single_alpha,
+                    args.use_corr,
+                )
             )
 
             wt, corrs, valphas, bscorrs, valinds = bootstrap_ridge(
@@ -121,12 +137,12 @@ if __name__ == "__main__":
                 delPstim,
                 zPresp,
                 alphas,
-                nboots,
-                chunklen,
-                nchunks,
-                singcutoff=singcutoff,
-                single_alpha=single_alpha,
-                use_corr=use_corr,
+                args.nboots,
+                args.chunklen,
+                args.nchunks,
+                singcutoff=args.singcutoff,
+                single_alpha=args.single_alpha,
+                use_corr=args.use_corr,
             )
 
             scores_list.append(corrs)
@@ -134,4 +150,6 @@ if __name__ == "__main__":
             best_alphas_list.append(valphas)
 
         mean_scores = np.mean(scores_list, axis=0)
+        sem_scores = sem(scores_list, axis=0)
         np.save(os.path.join(output_dir, "scores_mean.npy"), mean_scores)
+        np.save(os.path.join(output_dir, "scores_sem.npy"), sem_scores)
